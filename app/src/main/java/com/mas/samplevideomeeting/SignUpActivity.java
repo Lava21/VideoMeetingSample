eebc10c18cd9d6689d444bfbe85467558bcb8f6f
Lava21 <msyarif559@gmail.com>
2020-08-31 16:09:45 +0700

    update commit

--- app/src/main/java/com/mas/samplevideomeeting/SignUpActivity.java
+++ app/src/main/java/com/mas/samplevideomeeting/SignUpActivity.java
@@ -3,6 +3,7 @@ package com.mas.samplevideomeeting;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class SignUpActivity extends AppCompatActivity {
 
@@ -10,5 +11,19 @@ public class SignUpActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        findViewById(R.id.ivBack).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+
+        findViewById(R.id.tvSignIn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
     }
 }
